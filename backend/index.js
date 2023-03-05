b43e9ce5f573f623af1d7b435de7dac348a904a7
palaksharma23 <mahimasharma2312@gmail.com>
2023-03-05 15:28:11 +0530

    Resolved the CORS policy

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,7 @@ const mongoDB = require("./db");
 const PORT = 5000;
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://mealmate-backend.onrender.com"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
